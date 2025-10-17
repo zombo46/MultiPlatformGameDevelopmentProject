98f3f68b3c01355fa7064640821acb5ebafb20f9
Alex Arthur Camfield <a.camfield@se22.qmul.ac.uk>
2025-10-17 17:06:06 +0100

    Make basic enemy movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public Transform goal;
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+    
+}
