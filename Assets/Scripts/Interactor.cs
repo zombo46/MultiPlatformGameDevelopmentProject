7b392107b4b4877133f9afe813ef64ea3a8aa627
dorthpedro <darthb04@gmail.com>
2025-10-21 22:44:58 +0100

    Fixed a lot of stuff, added interactable class
    There were issues with the oxygen spawner, they are now fixed. Also added an interactable cube as a demonstration of an interaction button (E) - can be used for future interactable stuff.

---
+++ Assets/Scripts/Interactor.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IInteractable
+{
+    void Interact();
+}
+public class Interactor : MonoBehaviour
+{
+    public Transform interactionPoint;
+    public float interactionRange = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (interactionPoint == null)
+        {
+            interactionPoint = this.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Collider[] colliders = Physics.OverlapSphere(interactionPoint.position, interactionRange);
+            foreach (Collider collider in colliders)
+            {
+                IInteractable interactable = collider.GetComponent<IInteractable>();
+                if (interactable != null)
+                {
+                    interactable.Interact();
+                    break;
+                }
+            }
+        }
+    }
+}
